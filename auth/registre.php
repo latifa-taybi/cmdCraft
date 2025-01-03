0f8c52a174ae257b57be56c4fe84b16e07cb708a
latifa <taybilatifa46@gmail.com>
2025-01-03 14:30:12 +0100

    registre

--- auth/registre.php
+++ auth/registre.php
@@ -1,2 +1,26 @@
+<?php
+require_once '../config/config.php';
 
+if(isset($_POST['registre'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+
+    $db = new Database();
+    $pdo = $db->getConn();
+
+    $password_hashe = password_hash($password , PASSWORD_BCRYPT);
+
+    $stmt = $pdo->prepare("INSERT INTO users(name, email, mot_de_passe) VALUES(:name, :email, :mot_de_passe)");
+    if($stmt->execute([
+        ':name'=>$name,
+        ':email'=>$email,
+        ':mot_de_passe'=>$password_hashe
+    ])){
+        echo 'true';
+    }else{
+        echo 'false';
+    }
+}
+?>
 
