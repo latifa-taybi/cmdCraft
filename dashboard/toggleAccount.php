9f298fabf6a5b35269abcaa7f90adfa3391d0c78
latifa <taybilatifa46@gmail.com>
2025-01-08 15:35:32 +0100

    ajout au panier

--- dashboard/toggleAccount.php
+++ dashboard/toggleAccount.php
@@ -5,7 +5,7 @@ require_once '../config/config.php';
 if(isset($_GET['id'])){
     $db = new database;
     $pdo=$db->getConn();
-
+    
     $user = new User($pdo);
     $userId = $user->getUserId($_GET['id']);
     $toggleAcc = $user->accountManager($_GET['id']);
